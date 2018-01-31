566af82912ef255a6e12d91106408a6d1ecb7146
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-01-31 15:04:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -194,13 +194,7 @@ bot.on("message", function(message) {
 			message.channel.send(roast[Math.floor(Math.random() * roast.length)]);
 		break;
 	case "say":
-    if(message.author.id !== config.ownerID) return;
-    try {
-      const code = args.join(" ");
-      let evaled = eval(code);
-
-      if (typeof evaled !== "string")
-        evaled = require("util").inspect(evaled);
+		    message.channel.send("**In maintenance**")
 		break;
     }
 });

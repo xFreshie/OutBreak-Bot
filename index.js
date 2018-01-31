f52490df87cece791505387407cf495fc3db6773
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-01-31 21:23:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -198,12 +198,11 @@ bot.on("message", function(message) {
 		break;
 // Create an event listener for messages
 client.on('message', message => {
-	// If the message is "what is my avatar"
-  	if (message.content === 'what is my avatar') {
-    	// Send the user's avatar URL
-    		message.reply(message.author.avatarURL);
-
-    }
+  // If the message is "what is my avatar"
+  if (message.content === 'what is my avatar') {
+    // Send the user's avatar URL
+    message.reply(message.author.avatarURL);
+  }
 });
- 
+
 bot.login(process.env.BOT_TOKEN);

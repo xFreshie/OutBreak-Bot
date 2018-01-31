da0fd1f28dc0ec1263284d9d2b6c067ac65bc3de
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-01-31 21:15:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -194,8 +194,19 @@ bot.on("message", function(message) {
 			message.channel.send(roast[Math.floor(Math.random() * roast.length)]);
 		break;
 	case "say":
-		    message.channel.send(arg)
+		    message.channel.send("```css\nIn Maintenance```")
 		break;
+import random
+
+class Slapper(commands.Converter):
+    async def convert(self, ctx, argument):
+        to_slap = random.choice(ctx.guild.members)
+        return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
+
+@bot.command()
+async def slap(ctx, *, reason: Slapper):
+    await ctx.send(reason)
+
     }
 });
  

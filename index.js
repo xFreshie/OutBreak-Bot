1477b1cefac8805ac1c02f25f64d7055d9a7ac0e
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-01-31 14:43:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -195,10 +195,7 @@ bot.on("message", function(message) {
 		break;
 	case "say":
     			args = message.content.split(" ")
-    			args[0] = !say
-    			args[1] = Hey
-   			args[2] = There
-    			args[1:] = Hey There
+    			#args[1:] = Hey There
     			message.channel.send(message.channel, "%s" % (" ".join(args[1:])))
 		break;
     }

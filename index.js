c78d3c8d8ea512486e5ad67948462524d769342f
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-02-04 14:38:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -212,7 +212,7 @@ bot.on("message", function(message) {
 		    break;
 	case "love":
 		    if (!message.mentions.members.first())
-			    reply message.reply("Usage -love @mention")
+			    reply message.channel.send("Usage -love @mention")
 		    message.channel.send(message.author.username "loves" member.user.username);
 		    break;
     }	    

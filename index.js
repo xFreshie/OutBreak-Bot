fe5f75112000f736b16e6f0e45c37e6c77c245a8
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-02-05 15:18:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -210,21 +210,6 @@ bot.on("message", function(message) {
 	case "pizza":
 		    message.channel.send(":pizza:\n**Did you expect a pizza image? nah man**")
 		    break;
-	case "purge":
-	// This command removes all messages from all users in the channel, up to 100.
-    
-    // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-	.catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
-		    break;
   }
 });
 

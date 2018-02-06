df21a2171174287975b86d2223bfaefca031a6ff
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-02-06 12:46:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -210,9 +210,33 @@ bot.on("message", function(message) {
 	case "pizza":
 		    message.channel.send(":pizza:\n**Did you expect a pizza image? nah man**")
 		    break;
-	if (message === 'blah') {
- 	 message.channel.send('Meh.');
-	}
+let points = JSON.parse(fs.readFileSync("./points.json", "utf8"));
+        
+        client.on("message", message => {
+          if (!message.content.startsWith(prefix)) return;
+          if (message.author.bot) return;
+        
+          if (!points[message.author.id]) points[message.author.id] = {
+            points: 0,
+            level: 0
+          };
+          let userData = points[message.author.id];
+          userData.points++;
+        
+          let curLevel = Math.floor(0.1 * Math.sqrt(userData.points));
+          if (curLevel > userData.level) {
+            // Level up!
+            userData.level = curLevel;
+            message.reply(`You"ve leveled up to level **${curLevel}**! Ain"t that dandy?`);
+          }
+        
+          if (message.content.startsWith(prefix + "level")) {
+            message.reply(`You are currently level ${userData.level}, with ${userData.points} points.`);
+          }
+          fs.writeFile("./points.json", JSON.stringify(points), (err) => {
+            if (err) console.error(err)
+          });
+
   }
 });
 

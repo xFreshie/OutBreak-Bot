3c10ef9b2145d8f1c6a79c3dbdc9c0bf2e9580f4
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-02-10 00:27:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -228,17 +228,18 @@ bot.on("message", function(message) {
 		    message.channel.send(client.guilds.size)
 		    break;
 //Set Prefix commands | BETTER NOT TOUCH THIS
-	exports.run = (bot, msg, args) => {
-    if (args.length < 1) {
-        throw 'Please provide a prefix to set!';
-    }
+	    case "setprefix":
+   		 if (args.length < 1) {
+      		  throw 'Please provide a prefix to set!';
+   		 }
 
-    const prefix = args.join(' ');
-    bot.managers.config.set('prefix', prefix);
-    // No point trying to delete this message, the bot will be
-    // rebooting before we have a chance to.
-    msg.edit('Prefix set, rebooting! :ok_hand:');
-};
+   		 const prefix = args.join(' ');
+   		 bot.managers.config.set('prefix', prefix);
+   		 // No point trying to delete this message, the bot will be
+    		// rebooting before we have a chance to.
+   		 msg.edit('Prefix set, rebooting! :ok_hand:');
+		};
+			break;
     }	
 });		 	    
 bot.login(process.env.BOT_TOKEN);

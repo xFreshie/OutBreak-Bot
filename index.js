9f74a0a54a87b51356f65fb56fb5b643d28d16eb
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-02-10 00:29:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -229,16 +229,16 @@ bot.on("message", function(message) {
 		    break;
 //Set Prefix commands | BETTER NOT TOUCH THIS
 	    case "setprefix":
-   		 if (args.length < 1) {
-      		  throw 'Please provide a prefix to set!';
-   		 }
+		        if (args.length < 1) {
+        throw 'Please provide a prefix to set!';
+    }
 
-   		 const prefix = args.join(' ');
-   		 bot.managers.config.set('prefix', prefix);
-   		 // No point trying to delete this message, the bot will be
-    		// rebooting before we have a chance to.
-   		 msg.edit('Prefix set, rebooting! :ok_hand:');
-		};
+    const prefix = args.join(' ');
+    bot.managers.config.set('prefix', prefix);
+    // No point trying to delete this message, the bot will be
+    // rebooting before we have a chance to.
+    msg.edit('Prefix set, rebooting! :ok_hand:');
+};
     }	
 });		 	    
 bot.login(process.env.BOT_TOKEN);

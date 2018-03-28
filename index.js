5be0726d7605c07c7305bb3cf5ba37b7d09c56a5
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-28 20:17:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -285,13 +285,5 @@ client.on("message", (message) => {
   if(message.content === "ayy") {
     message.channel.send("Ayy, lmao!");
   }
-  if(message.content === "wat") {
-    message.channel.send("Say what?");
-  }
-  if(message.content === "lol") {
-    message.channel.send("roflmaotntpmp");
-  }
-
 });
-
 client.login(process.env.BOT_TOKEN);

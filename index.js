5ea19e1624e9dfc3db3b3ecb9817c457eef5b367
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-28 20:11:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -198,16 +198,7 @@ var wot = [
     if(!message.member.roles.some(r=>["[Admin]", "[Moderator]", "[Trial Mod]", "[Jr. Mod]", "~Discord Manager~"].includes(r.name)) )
       return message.reply("Sorry, you don't have permissions to use this!");
     // get the delete count, as an actual number.
-    const deleteCount = parseInt(args[0], 10);
-    
-    // Ooooh nice, combined conditions. <3
-    if(!deleteCount || deleteCount < 2 || deleteCount > 100)
-      return message.reply("Please provide a number between 2 and 100 for the number of messages to delete");
-    
-    // So we get our messages, and delete them. Simple enough, right?
-    const fetched = await message.channel.fetchMessages({count: deleteCount});
-    message.channel.bulkDelete(fetched)
-      .catch(error => message.reply(`Couldn't delete messages because of: ${error}`));
+	  message.channel.send("Purge has been disabled due to it purging days of messages instead of the given amount of messages.");
   }
   
   if(command === "8ball") {

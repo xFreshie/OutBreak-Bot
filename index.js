f163b555c46b43a4fcddeacec3ea58b66e831ac7
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-29 15:54:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -207,13 +207,37 @@ var wot = [
   }
   
   if(command === "userinfo") {
-            var embed = new Discord.RichEmbed()
-            .setAuthor(message.author.username)
-            .setDescription("This is the user's info!")
-            .setColor("#9B59B6")
-            .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
-            .addField("ID", message.author.id)
-            .addField("Created At", message.author.createdAt);
+const moment = require("moment");
+require("moment-duration-format");
+const status = {
+  online: "Online",
+  idle: "Idle",
+  dnd: "Do Not Disturb",
+  offline: "Offline/Invisible"
+};
+const randomColor = "#000000".replace(/0/g, function () { return (~~(Math.random() * 16)).toString(16); });
+exports.run = (client, msg, args) => {
+  const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
+  if (!member) return msg.reply("Please provide a vaild Mention or USER ID");
+  let bot;
+  if (member.user.bot === true) {
+    bot = "Yes";
+  } else {
+    bot = "No";
+  }
+  const embed = new Discord.MessageEmbed()
+    .setColor(randomColor)
+    .setThumbnail(`${member.user.displayAvatarURL()}`)
+    .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
+    .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
+    .addField("Bot?", `${bot}`, true)
+    .addField("Guild", `${bot}`, true)
+    .addField("Status", `${status[member.user.presence.status]}`, true)
+    .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "not playing anything."}`, true)
+    .addField("Roles", `${member.roles.filter(r => r.id !== msg.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+    .addField("Joined At", `${moment.utc(member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
+    .addField("Created At", `${moment.utc(member.user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
+
         message.channel.send(embed);
   }
   

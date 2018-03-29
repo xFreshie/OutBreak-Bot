7faa902efafa2b05edc74dc608b06693ee4c8e87
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-29 17:15:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ client.on("ready", () => {
   console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
   // Example of changing the bot's playing game to something useful. `client.user` is what the
   // docs refer to as the "ClientUser".
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.activity(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildCreate", guild => {
   // This event triggers when the bot joins a guild.
   console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
-  client.user.setGame(`on ${client.guilds.size} servers`);
+  client.user.activity(`on ${client.guilds.size} servers`);
 });
 
 client.on("guildDelete", guild => {

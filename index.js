4554a7b27675c81612d00bc27bbcd74bbcb1cfed
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-29 16:12:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -207,19 +207,13 @@ var wot = [
   }
   
   if(command === "userinfo") {
-  const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
-  if (!member) return msg.reply("Please provide a vaild Mention or USER ID");
-  const embed = new Discord.MessageEmbed()
-    .setColor(randomColor)
-    .setThumbnail(`${member.user.displayAvatarURL()}`)
-    .setAuthor(`${member.user.tag} (${member.id})`, `${member.user.avatarURL()}`)
-    .addField("Nickname:", `${member.nickname !== null ? `Nickname: ${member.nickname}` : "No nickname"}`, true)
-    .addField("Status", `${member.user.presence.status}`, true)
-    .addField("Playing", `${member.user.presence.game ? `${member.user.presence.game.name}` : "not playing anything."}`, true)
-    .addField("Roles", `${member.roles.filter(r => r.id !== msg.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
-    .addField("Joined At", `${member.joinedAt.format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true)
-    .addField("Created At", `${member.user.createdAt.format("dddd, MMMM Do YYYY, HH:mm:ss")}`, true);
-
+            var embed = new Discord.RichEmbed()
+            .setAuthor(message.author.username)
+            .setDescription("This is the user's info!")
+            .setColor("#9B59B6")
+            .addField("Full Username", `${message.author.username}#${message.author.discriminator}`)
+            .addField("ID", message.author.id)
+            .addField("Created At", message.author.createdAt);
         message.channel.send(embed);
   }
   
@@ -285,6 +279,34 @@ var wot = [
   
   if (command === "staffcommands") {
     message.channel.send("**=-=-=-Staff Help=-=-=-=**\n-kick \n -ban \n-purge\n ``These are the only commands for now.``");
+  }
+	
+  if (command === "mute") {
+  let reason = args.slice(1).join(' ');
+  let member = message.mentions.members.first();
+  let modlog = message.guild.channels.find('name', 'mod-log');
+  let muteRole = message.guild.roles.find('name', '[Muted]');
+  if (!modlog) return message.reply('I cannot find a mod-log channel').catch(console.error);
+  if (!muteRole) return message.reply('I cannot find a mute role').catch(console.error);
+  if (reason.length < 1) return message.reply('You must supply a reason for the mute.').catch(console.error);
+  if (message.mentions.users.size < 1) return message.reply('You must mention someone to mute them.').catch(console.error);
+  const embed = new Discord.RichEmbed()
+    .setColor(0x00AE86)
+    .setTimestamp()
+    .setDescription(`**Action:** Un/mute\n**Target:** ${member.user.tag}\n**Moderator:** ${message.author.tag}\n**Reason:** ${reason}`);
+
+  if (!message.guild.me.hasPermission('MANAGE_ROLES')) return message.reply('I do not have the correct permissions.').catch(console.error);
+
+  if (member.roles.has(muteRole.id)) {
+    member.removeRole(muteRole).then(() => {
+      client.channels.get(modlog.id).send({embed}).catch(console.error);
+    })
+    .catch(e=>console.error("Cannot remove muted role: " + e));
+  } else {
+    member.addRole(muteRole).then(() => {
+      client.channels.get(modlog.id).send({embed}).catch(console.error);
+    })
+    .catch(e=>console.error("Cannot add muted role: " + e));
   }
 });
 

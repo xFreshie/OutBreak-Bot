4e2081740e26c9884048889ba5fbd3dac437c558
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-30 01:10:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -333,6 +333,39 @@ var wot = [
 	  let useer = message.mentions.users.first() ? message.mentions.users.first() : message.author
 	  message.channel.send(message.author.username + " **(　-_･) ︻デ═一 ▸**" + useer.username);
   }
+  if(command === "minecraftskin") {
+constructor(client) {
+		super(client, {
+			name: 'skin',
+			group: 'minecraft',
+			memberName: 'skin',
+			description: 'Get the skin of a Minecraft user',
+			aliases: ['minecraft-skin', 'mc-skin'],
+			examples: ['face Notch'],
+			throttling: {
+				usages: 1,
+				duration: 3
+			},
+			args: [{
+				key: 'username',
+				prompt: 'What user do you want to look up?',
+				type: 'string'
+			}]
+		});
+	}
+
+	run(msg, { username }) {
+		return msg.reply({
+			embed: {
+				author: {
+					name: username,
+					// eslint-disable-next-line camelcase
+					icon_url: `https://minotar.net/helm/${encodeURIComponent(username)}`
+				},
+				image: { url: `https://minotar.net/skin/${encodeURIComponent(username)}` }
+			}
+		});
+	}
 });
 
 client.login(process.env.BOT_TOKEN);

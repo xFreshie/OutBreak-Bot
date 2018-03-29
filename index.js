b0aae39d2817f61e52cf28038fcc683ba3068715
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-03-29 23:04:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -297,18 +297,9 @@ var wot = [
         file: (nou[Math.floor(Math.random() * nou.length)])
     });
   }
-  if(command === "shoot") {
-    if (msg.mentions.users.size < 1) {
-        throw '@mention some people to shoot!';
-    }
-
-    let output = msg.mentions.users.map(m => `**${m}** :gun:`).join('\n');
-
-    msg.delete();
-    msg.channel.send({
-        embed: bot.utils.embed(`${bot.user.username} is on a killing spree!`, output)
-    });
-};
+  if(command === "ships") {
+	  message.channel.send("**Color x ALAND** ``COLAND``\n**EnderKawaii x Nicolidas** ``Enderlidas``\n**Iza x Cleaner** ``Cleanza``\nAND FINALLY! **Fresh x no one** ``#lonely4ever``");
+  }
 });
 
 client.login(process.env.BOT_TOKEN);

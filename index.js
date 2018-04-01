53ac83b04eb9816cf94b04f8b954f7e184f0a0c7
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-01 20:01:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -429,6 +429,22 @@ var wot = [
       return message.reply("Sorry, you don't have permissions to use this!");
 	  message.channel.send("**=-=-= FrostRealms Support Team =-=-=**\n-ty\n-move\n-proof\n-notpunishable\n-mod");
   }
+  if(command === "getsomehelp") {
+    try {
+        await message.guild.fetchMember(message.author);
+        await message.guild.fetchMember(bot.user);
+    } catch(e) {
+        return send("**Something went wrong.**");
+    }
+    let member = message.member;
+    if (!member.voiceChannel) return send("**You must be in a voice channel for this command.**")
+    if (message.guild.member(bot.user).voiceChannel) return send("**Im already playing something.**")
+
+    member.voiceChannel.join().then(voiceConnection => {
+        voiceConnection.playFile(".Nitro/images/stopit.mp3")
+        setTimeout(() => voiceConnection.disconnect(), 4000)
+    }).catch(console.log)
+}
 });
 
 client.login(process.env.BOT_TOKEN);

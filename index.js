82362c014afe0ef07850687b59efc6a5fcae9fb2
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-04 00:03:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -525,6 +525,23 @@ if(command === "pathi") {
 		file: (pathii[Math.floor(Math.random() * pathii.length)])
 	});
 }
+if(command === "discrim") {
+    if (!message.args[0]) message.args[0] = message.author.discriminator
+    if (message.args[0].length !== 4) return send("Discriminators are 4 numbers")
+    let disc = message.args[0].replace(/[^0123456789]/g, "")
+    if (disc.length !== 4) return send("Discriminators are 4 numbers")
+
+    client.shard.broadcastEval("this.users.filter(u => u.discriminator === '"+disc+"').map(u => u.username)").then(ret => {
+        for (i=0;i<ret.length-1;i++) {
+            ret[0].concat(ret[i+1])
+        }
+        let filt = {}
+        for (i=0;i<ret[0].length;i++) {
+            filt[ret[0][i]] = true
+        }
+        filt = Object.keys(filt)
+        message.channel.send("**Users with the Discriminator: "+disc+"**\n"+filt.join(", "))
+    })
 });
 
 client.login(process.env.BOT_TOKEN);

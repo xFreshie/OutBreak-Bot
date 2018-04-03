a7656fbcc4add365659a746fdafa9e9a80816d2b
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-03 15:23:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -476,6 +476,20 @@ if (message.content === "iza") {
 if(command === "vaughn") {
 	message.channel.send("no u*");
 }
+if(command === "dmfresh") {
+	var message = payload.message
+        var suffix = message.content.split(" ").slice(1).join(" ");
+        if(suffix == "" || suffix == null) return message.channel.sendMessage("Do " +config.prefix+ "dmfresh <Message?> to Message Fresh Privately!");
+        let embed = new Discord.RichEmbed();
+        embed.setColor(0x9900FF)
+        embed.setThumbnail(message.author.displayAvatarURL)
+        embed.addField("Message: ", suffix, true)
+        embed.addField("Discord Channel: ", message.guild.name, true)
+        embed.addField("By: ", message.author.username, true)
+
+        bot.client.users.get(config.owner_id).send({embed})
+    })
+}
 });
 
 client.login(process.env.BOT_TOKEN);

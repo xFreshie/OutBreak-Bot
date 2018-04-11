0a7aeee1fad0acd0d10b8867d52ec892e0a75fd5
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-11 17:41:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -575,35 +575,8 @@ if(command === "goodlaughing") {
 }
 if(command === "addrole") {
 	// Permissions
-}
-  CREATE_INSTANT_INVITE: true,
-  KICK_MEMBERS: true,
-  BAN_MEMBERS: true,
-  ADMINISTRATOR: true,
-  MANAGE_CHANNELS: true,
-  MANAGE_GUILD: true,
-  ADD_REACTIONS: true,
-  READ_MESSAGES: true,
-  SEND_MESSAGES: true,
-  SEND_TTS_MESSAGES: true,
-  MANAGE_MESSAGES: true,
-  EMBED_LINKS: true,
-  ATTACH_FILES: true,
-  READ_MESSAGE_HISTORY: true,
-  MENTION_EVERYONE: true,
-  EXTERNAL_EMOJIS: true,
-  CONNECT: true,
-  SPEAK: true,
-  MUTE_MEMBERS: true,
-  DEAFEN_MEMBERS: true,
-  MOVE_MEMBERS: true,
-  USE_VAD: true,
-  CHANGE_NICKNAME: true,
-  MANAGE_NICKNAMES: true,
-  MANAGE_ROLES_OR_PERMISSIONS: true,
-  MANAGE_WEBHOOKS: true,
-  MANAGE_EMOJIS: true
-}
+		if(!message.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS'))
+		return message.reply("You can't add roles.. guys, don't be silly. :/ !");
       // Check if the message mentions a user.
       if (message.mentions.members.size === 0) return message.reply('Please mention a user to give the role to.');
       const member = message.mentions.members.first();

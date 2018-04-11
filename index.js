f7f3fcaf95b22012daa0be22024abf44f9c86efb
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-11 13:30:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ bot.on("message", function(message) {
             message.channel.send(response.output).catch(console.error);
             message.channel.stopTyping();
           }, Math.random() * (1 - 3) + 1 * 1000);
-        });
+	})
     }
     //-----------------CHAT MODA FKIN BOT-----------------  Better not Touch this  Better not Touch this
     // --------------------------------------------

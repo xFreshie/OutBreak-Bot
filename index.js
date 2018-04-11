34477ef7f4e5effc3de887e7c5170773471e1ccb
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-12 01:48:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -363,11 +363,6 @@ if(!message.member.roles.some(r=>["[Admin]", "[Moderator]", "[Trial Mod]", "[Jr.
     });
   }
   
-  if(command === "roast") {
-	  let useeer = message.mentions.users.first() ? message.mentions.users.first() : message.author
-	  message.channel.send(`<@${message.mentions.users.first().id}>` +", " + roast[Math.floor(Math.random() * roast.length)]);
-  }
-  
   if(command === "whosyourdada") {
     message.channel.send("who's your brudda, does he kno da wae?");
   }
@@ -630,7 +625,7 @@ if(command === "discrim") {
     message.channel.send({embed})
       }
 }
-if(command === "test") {
+if(command === "roast") {
 const start = ['a lazy', 'a stupid', 'an insecure', 'an idiotic', 'a slimy', 'a slutty', 'a smelly', 'a pompous', 'a communist', 'a dicknose', 'a pie-eating', 'a racist', 'an elitist', 'a white trash', 'a drug-loving', 'a butterface', 'a tone deaf', 'a ugly', 'a creepy', 'an artless', 'a bawdy', 'a beslubbering', 'a bootless', 'a churlish', 'a cockered', 'a clouted', 'a craven', 'a currish', 'a dankish', 'a dissembling', 'a droning', 'an errant', 'a fawning', 'a fobbing', 'a frothy', 'a gleeking', 'a goatfish', 'a gorbellied', 'an impertinent', 'an infectious', 'a jarring', 'a loggerheaded', 'a lumpish', 'a mammering', 'a mangled', 'a mewling', 'a paunchy', 'a pribbling', 'a puking', 'a puny', 'a qualling', 'a rank', 'a reeky', 'a roguish', 'a ruttish', 'a saucy', 'a spleeny', 'a spongy', 'a surly', 'a tottering', 'an unmuzzled', 'a vain', 'a venomed', 'a villainous', 'a warped', 'a wayward', 'a weedy', 'a yeasty', 'a lilly-livered', 'a rotten', 'a stinky', 'a lame', 'a dim-witted', 'a funky', 'a crusty', 'a steamy', 'a drizzly', 'a grizzly', 'a squirty', 'an uptight', 'a hairy', 'a husky', 'an arrogant', 'a nippy', 'a chunky', 'a smelly', 'a drooling', 'a crusty', 'a decrepic', 'a stupid', 'a moronic', 'a greasy', 'a poxy', 'an ugly', 'a smelly', 'a putrid', 'a shitty', 'an assinine', 'a sickening']
 
 const middle = ['douche', 'ass', 'turd', 'rectum', 'butt', 'cock', 'shit', 'crotch', 'bitch', 'turd', 'prick', 'slut', 'taint', 'fuck', 'dick', 'boner', 'shart', 'nut', 'sphincter', 'base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed', 'clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-gripping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motly-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-red', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten', 'hiney', 'poop', 'toot', 'wedgie', 'stool', 'fudge', 'bum', 'potty', 'dookie', 'pudding', 'sphincter', 'booger', 'feces', 'snot', 'crust', 'badonk-a', 'crud', 'sludge', 'tool', 'shit-kicking', 'monkey-licking', 'butt-munching', 'crotch-sniffing', 'donkey-spanking', 'fashion-illiterate', 'worm-ridden', 'grub-fucking', 'lathered-up', 'pasty-waisted', 'snot-flicking', 'fart-eating']

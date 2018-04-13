41dd093120646f0166aadb76b4f593e5d0c24b4a
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-13 15:44:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -638,5 +638,24 @@ if(command === "uptime") {
         strDate += date.getUTCSeconds() + ' seconds**';
         message.channel.send('UPTIME:\n' + strDate);
     }
+if(command === "timeuntil") {
+            var d1 = new Date(msg.content);
+            var d2 = new Date();
+            var t1 = d1.getTime();
+            var t2 = d2.getTime();
+            var time = t1 - t2;
+            var years = parseInt(time / 31536000000);
+            var totalDays = parseInt(time / 86400000);
+            var days = parseInt((time - (years * 31536000000)) / 86400000);
+            var totalHours = parseInt(time / 3600000);
+            var hours = parseInt((time - (totalDays * 86400000)) / 3600000);
+            var totalMinutes = parseInt(time / 60000);
+            var minutes = parseInt((time - (totalHours * 3600000)) / 60000);
+            var seconds = parseInt((time - (totalMinutes * 60000)) / 1000);
+            msg.channel.send('Time until ' + d1.toDateString() + ': \n**' + years + ' years, ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + ' seconds**');
+        } catch (err) {
+            msg.channel.send('Error processing date. Please put in MM/DD/YY format');
+        }
+}
 });
 client.login(process.env.BOT_TOKEN);

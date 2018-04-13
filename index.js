f1fe8994f528964999b2920132971e6900fa9e26
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-13 15:52:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -654,5 +654,21 @@ if(command === "timeuntil") {
             var seconds = parseInt((time - (totalMinutes * 60000)) / 1000);
             message.channel.send('Time until ' + d1.toDateString() + ': \n**' + years + ' years, ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + ' seconds**');
 }
+if(command === "timesince") {
+            var d1 = new Date(message.content);
+            var d2 = new Date();
+            var t1 = d1.getTime();
+            var t2 = d2.getTime();
+            var time = t2 - t1;
+            var years = parseInt(time / 31536000000);
+            var totalDays = parseInt(time / 86400000);
+            var days = parseInt((time - (years * 31536000000)) / 86400000);
+            var totalHours = parseInt(time / 3600000);
+            var hours = parseInt((time - (totalDays * 86400000)) / 3600000);
+            var totalMinutes = parseInt(time / 60000);
+            var minutes = parseInt((time - (totalHours * 3600000)) / 60000);
+            var seconds = parseInt((time - (totalMinutes * 60000)) / 1000);
+	    message.channel.send('Time since ' + d1.toDateString() + ': \n**' + years + ' years, ' + days + ' days, ' + hours + ' hours, ' + minutes + ' minutes, and ' + seconds + ' seconds**');
+}
 });
 client.login(process.env.BOT_TOKEN);

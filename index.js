d8bab0400214ef947d08e6e1bf874b94665bb924
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-13 23:04:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -713,33 +713,6 @@ if(command === "blacklist") {
             message.channel.send('User ID ' + msg.content + ' has been blacklisted!');
         }
     }
-if(command === "reminder") {
-const { MessageEmbed } = require("discord.js");
-const FNite = require("fortnite");
-const fortnite = new FNite(process.env.TRACKER);
-    if (!args.length) return message.response("❗", `Invalid Usage, please do:\`${this.help.usage}\``);
-      const data = await fortnite.getInfo(args.splice(1).join(" "), args[0]);
-      message.channel.send(new MessageEmbed()
-        .setTitle(`${data.username}, on ${data.platformNameLong}`)
-        .setURL(`https://partybus.gg/player/${data.username}`)
-        .addField("Squads", `**Top 6 :** ${data.lifetimeStats[3].value}\n**Top 3 :** ${data.lifetimeStats[2].value}\n**Wins:** ${data.stats.p9.top1.value}\n**KD:** ${data.stats.p9.kd.value}\n**Matches Played:** ${data.stats.p9.matches.value}\n**Kills:** ${data.stats.p9.kills.value}\n**Kills Per Game:** ${data.stats.p9.kpg.value}`, true)
-        .addField("Duo", `**Top 12 :** ${data.lifetimeStats[4].value}\n**Top 5 :** ${data.lifetimeStats[1].value}\n**Wins:** ${data.stats.p10.top1.value}\n**KD:** ${data.stats.p10.kd.value}\n**Matches Played:** ${data.stats.p10.matches.value}\n**Kills:** ${data.stats.p10.kills.value}\n**Kills Per Game:** ${data.stats.p10.kpg.value}`, true)
-        .addField("Solo", `**Top 25 :** ${data.lifetimeStats[5].value}\n**Top 10 :** ${data.lifetimeStats[0].value}\n**Wins:** ${data.stats.p2.top1.value}\n**KD:** ${data.stats.p2.kd.value}\n**Matches Played:** ${data.stats.p2.matches.value}\n**Kills:** ${data.stats.p2.kills.value}\n**Kills Per Game:** ${data.stats.p2.kpg.value}`, true)
-        .addBlankField()
-        .addField("Score", data.lifetimeStats[6].value, true)
-        .addField("Matches", data.lifetimeStats[7].value, true)
-        .addField("Total Wins", data.lifetimeStats[8].value, true)
-        .addField("Win Rate", data.lifetimeStats[9].value, true)
-        .addField("Kills", data.lifetimeStats[10].value, true)
-        .addField("KDR", data.lifetimeStats[11].value, true)
-        .addField("Kills Per Minute", data.lifetimeStats[12].value, true)
-        .addField("Time Played", data.lifetimeStats[13].value, true)
-        .addField("Average Survival Time", data.lifetimeStats[14].value, true)
-        .setColor(message.guild.me.roles.highest.color || 5198940));
-      
-    } else {
-      message.channel.send(`Player Not Found or invalid form type, for correct usage do: \`${this.help.usage}\`.`);
-    }
-})
+});
 client.login(process.env.BOT_TOKEN);
 //Restart, uptime, timeuntil, triggered, timesince, blacklist and guilds commands were used from RoBot, link to the source > https://github.com/mcao/RoBot/tree/master/modules

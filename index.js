dca05ded0c9da85f237d7a9212590caffe90d4e2
xFreshie <35957398+xFreshie@users.noreply.github.com>
2018-04-13 22:52:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -714,33 +714,32 @@ if(command === "blacklist") {
         }
     }
 if(command === "reminder") {
-    if (args.length === 0) {
-      const reminders = message.member.reminders;
-      if (reminders.length === 0) return message.response(undefined, "You do not have any reminders set.");
-      else return message.channel.send("**Your Reminders:**\n" + reminders.map(r => `${r.reminder} - ${moment(r.reminderTimestamp).fromNow()}`).join("\n"));
-    }
-    const blah = await this.regCheck(args.join(" "));
-    if (!blah) return message.respond(undefined, "Invalid Command usage, you must supply a reminder message and duration e.g; `Do the laundry in 20 minutes`.");
-    this.client.reminders.set(`${message.author.id}-${message.createdTimestamp + ms(blah.split("#")[1])}`, {
-      id: message.author.id,
-      guildid: message.guild.id,
-      reminder: blah.split("#")[0],
-      reminderTimestamp: message.createdTimestamp + ms(blah.split("#")[1])
-    });
-
-    message.channel.send(`I will remind you to \`${blah.split("#")[0]}\`, ${blah.split("#")[1]} from now.`);
-  }
-
-  regCheck(reminder) {
-    const remind = /(?:^| )(?:in ?)?(((?:\d{1,2}(?:\.\d|\d)?)|a) ?((?:m(?:in(?:ute)?)?|h(?:our)?|d(?:ay)?|w(?:eek)?|m(?:onth)?|y(?:ear)?)s?))\b/gi.exec(reminder);
-    if (!remind) return false;
-    const time = remind[1]
-      .replace(/ ms?\b/, " min") //m => min
-      .replace(/\ba ?((?:m(?:in(?:ute)?)?|h(?:our)?|d(?:ay)?|w(?:eek)?|m(?:onth)?|y(?:ear)?)s?)\b/g, "1 $1").trim(); // a "something" => 1 "something"
-    const input = /(?:me ?)?(?:to ?)?(.*)/gi.exec(reminder)[1]
-      .replace(remind[0], "").trim();
-    if (input.length === 0) return false;
-    return `${input}#${time}`;
+const { MessageEmbed } = require("discord.js");
+const FNite = require("fortnite");
+const fortnite = new FNite(process.env.TRACKER);
+    if (!args.length) return message.response("❗", `Invalid Usage, please do:\`${this.help.usage}\``);
+      const data = await fortnite.getInfo(args.splice(1).join(" "), args[0]);
+      message.channel.send(new MessageEmbed()
+        .setTitle(`${data.username}, on ${data.platformNameLong}`)
+        .setURL(`https://partybus.gg/player/${data.username}`)
+        .addField("Squads", `**Top 6 :** ${data.lifetimeStats[3].value}\n**Top 3 :** ${data.lifetimeStats[2].value}\n**Wins:** ${data.stats.p9.top1.value}\n**KD:** ${data.stats.p9.kd.value}\n**Matches Played:** ${data.stats.p9.matches.value}\n**Kills:** ${data.stats.p9.kills.value}\n**Kills Per Game:** ${data.stats.p9.kpg.value}`, true)
+        .addField("Duo", `**Top 12 :** ${data.lifetimeStats[4].value}\n**Top 5 :** ${data.lifetimeStats[1].value}\n**Wins:** ${data.stats.p10.top1.value}\n**KD:** ${data.stats.p10.kd.value}\n**Matches Played:** ${data.stats.p10.matches.value}\n**Kills:** ${data.stats.p10.kills.value}\n**Kills Per Game:** ${data.stats.p10.kpg.value}`, true)
+        .addField("Solo", `**Top 25 :** ${data.lifetimeStats[5].value}\n**Top 10 :** ${data.lifetimeStats[0].value}\n**Wins:** ${data.stats.p2.top1.value}\n**KD:** ${data.stats.p2.kd.value}\n**Matches Played:** ${data.stats.p2.matches.value}\n**Kills:** ${data.stats.p2.kills.value}\n**Kills Per Game:** ${data.stats.p2.kpg.value}`, true)
+        .addBlankField()
+        .addField("Score", data.lifetimeStats[6].value, true)
+        .addField("Matches", data.lifetimeStats[7].value, true)
+        .addField("Total Wins", data.lifetimeStats[8].value, true)
+        .addField("Win Rate", data.lifetimeStats[9].value, true)
+        .addField("Kills", data.lifetimeStats[10].value, true)
+        .addField("KDR", data.lifetimeStats[11].value, true)
+        .addField("Kills Per Minute", data.lifetimeStats[12].value, true)
+        .addField("Time Played", data.lifetimeStats[13].value, true)
+        .addField("Average Survival Time", data.lifetimeStats[14].value, true)
+        .setColor(message.guild.me.roles.highest.color || 5198940));
+      
+    } else {
+      message.channel.send(`Player Not Found or invalid form type, for correct usage do: \`${this.help.usage}\`.`);
+    }   
 }
 });
 client.login(process.env.BOT_TOKEN);
